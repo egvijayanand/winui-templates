7d5828096145aaf01a7e07d934595ff25142f729
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2023-03-03 01:46:23 +0530

    Templates updated to support .NET 8 Previews

--- src/WinUITemplatesCLI/Uninstall-Template.bat
+++ src/WinUITemplatesCLI/Uninstall-Template.bat
@@ -21,6 +21,8 @@ call Info "Uninstalling the %packageName% template ..."
 
 dotnet new uninstall %packageName%
 
+dotnet new --debug:rebuildcache
+
 if %errorlevel% == 0 (call Success "Process completed.") else (call Error "Failed to uninstall the template package.")
 
 :end
