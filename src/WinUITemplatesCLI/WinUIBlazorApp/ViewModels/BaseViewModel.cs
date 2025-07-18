ac2756603135ec6426eb589cbecf0f4eb4ce2af0
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2025-07-18 19:08:44 +0530

    Ability to abstract MAUI and Razor components as a separate class library in the WinUI Blazor project template #30

--- src/WinUITemplatesCLI/WinUIBlazorApp/ViewModels/BaseViewModel.cs
+++ src/WinUITemplatesCLI/WinUIBlazorApp/ViewModels/BaseViewModel.cs
@@ -1,13 +1,19 @@
 ﻿namespace WinUIBlazorApp._1.ViewModels
 {
-    public partial class BaseViewModel : ObservableObject
+    public partial class BaseViewModel(string title = "") : ObservableObject
     {
-        public BaseViewModel()
-        {
-
-        }
-
+#if (MauiLib || Net8)
         [ObservableProperty]
-        private string? _title;
+        private string _title = title;
+#else
+        private string _title = title;
+
+        // .NET MAUI SG is yet to support source-generated properties.
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+#endif
     }
 }
---
+++ src/WinUITemplatesCLI/WinUIBlazorApp/ViewModels/MauiViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace WinUIBlazorApp._1.ViewModels
+{
+    public partial class MauiViewModel() : BaseViewModel("Home")
+    {
+#if (MauiLib || Net8)
+        [ObservableProperty]
+        private string _startPath = "/counter";
+#else
+        private string _startPath = "/counter";
+
+        // .NET MAUI SG is yet to support source-generated properties.
+        public string StartPath
+        {
+            get => _startPath;
+            set => SetProperty(ref _startPath, value);
+        }
+#endif
+    }
+}
